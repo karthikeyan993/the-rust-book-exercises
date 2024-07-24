43fa3b7b66cd9b60ecc9426dc7c5fbbf2e120634
karthikeyan993 <karthiikkeyan@gmail.com>
2024-07-24 21:45:42 +0530

    removed target folder

--- temperature/target/debug/deps/temperature-e380147339354381.d
+++
@@ -1,5 +0,0 @@
-/home/karthikeyan/Documents/rust/exercises/temperature/target/debug/deps/temperature-e380147339354381: src/main.rs
-
-/home/karthikeyan/Documents/rust/exercises/temperature/target/debug/deps/temperature-e380147339354381.d: src/main.rs
-
-src/main.rs:
--- temperature/target/debug/temperature.d
+++
@@ -1,1 +0,0 @@
-/home/karthikeyan/Documents/rust/exercises/temperature/target/debug/temperature: /home/karthikeyan/Documents/rust/exercises/temperature/src/main.rs
