68e6337bedee7ac56c7da140a2f9ead55e865505
karthikeyan993 <karthiikkeyan@gmail.com>
2024-07-26 22:10:30 +0530

    added user input to temperature and fibonacci exercises

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,9 +1,45 @@
+use std::io;
+
 fn main() {
-    fibonacci(1);
-    fibonacci(2);
-    fibonacci(0);
-    fibonacci(4);
-    fibonacci(11);
+    'fibo_loop: loop {
+        println!(
+            "Please enter which Fibonacci number you'd like to calculate (e.g., 1st, 10th, etc.):"
+        );
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read the line");
+
+        let number: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter number between 1 - 100");
+                continue;
+            }
+        };
+        if number > 100 {
+            println!("Please enter number between 1 - 100");
+            continue 'fibo_loop;
+        }
+        fibonacci(number);
+        loop {
+            println!("c - continue, q - quit");
+            let mut input2 = String::new();
+            io::stdin()
+                .read_line(&mut input2)
+                .expect("Failed to read the line");
+
+            let exit: char = match input2.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            if exit == 'q' {
+                break 'fibo_loop;
+            } else if exit == 'c' {
+                continue 'fibo_loop;
+            }
+        }
+    }
 }
 
 fn fibonacci(n: u32) {
@@ -18,11 +54,11 @@ fn fibonacci(n: u32) {
         }
     } else {
         let mut index = 3;
-        let mut first = 0;
-        let mut second = 1;
+        let mut first: u128 = 0;
+        let mut second: u128 = 1;
         while index <= n {
             // Adding first and second and assigning the second to first and sum to second number
-            let mut _sum = 0;
+            let mut _sum: u128 = 0;
             _sum = first + second;
             first = second;
             second = _sum;
--- temperature/src/main.rs
+++ temperature/src/main.rs
@@ -1,15 +1,60 @@
+use std::io;
 fn main() {
     // Pass temperature and to what it should be converted in format 'C' for converting celsius and 'F' for farenheit covnersion
-    convert(100.0, 'C');
-    convert(38.6, 'F');
-    convert(38.6, 'Z');
+    // convert(100.0, 'C');
+    // convert(38.6, 'F');
+    // convert(38.6, 'Z');
+    'temp_loop: loop {
+        println!("Please enter the temperature value: ");
+        let mut input1 = String::new();
+        io::stdin()
+            .read_line(&mut input1)
+            .expect("Failed to read line");
+
+        let temp: f32 = match input1.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Please '1' for C->F, '2' for F->C");
+
+        let mut input2 = String::new();
+        io::stdin()
+            .read_line(&mut input2)
+            .expect("Failed to read the line");
+        let to: u8 = match input2.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        convert(temp, to);
+        loop {
+            let mut input3 = String::new();
+
+            println!("c - continue, q - quit");
+
+            io::stdin()
+                .read_line(&mut input3)
+                .expect("Failed to read line");
+
+            let exit: char = match input3.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+            if exit == 'q' {
+                break 'temp_loop;
+            } else if exit == 'c' {
+                continue 'temp_loop;
+            }
+        }
+    }
 }
 
-fn convert(temp: f32, to: char) {
-    if to == 'C' {
+fn convert(temp: f32, to: u8) {
+    if to == 2 {
         let result = (temp - 32.0) / 1.8;
         println!("{temp} F is {result} C");
-    } else if to == 'F' {
+    } else if to == 1 {
         let result = (temp * 1.8) + 32.0;
         println!("{temp} C is {result} F");
     } else {
